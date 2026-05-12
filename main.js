65c3034fea01dd07369275143300bdb27d338c3b
Andreas Granér <andreas@graner.me>
2026-05-12 10:26:28 +0200

    chore: add prettierrc with tabs to match original code style

--- main.js
+++ main.js
@@ -1,132 +1,132 @@
 // ----- Start of the assigment ----- //
 
 class ParticleSystem extends PIXI.Container {
-  constructor() {
-    super();
-    // Set start and duration for this effect in milliseconds
-    this.start = 0;
-    // Create a sprite
-    let sp = game.sprite("CoinsGold000");
-    // Set pivot to center of said sprite
-    sp.pivot.x = sp.width / 2;
-    sp.pivot.y = sp.height / 2;
-    // Add the sprite particle to our particle effect
-    this.addChild(sp);
-    // randomize startposition, scale, arc and speed on coin animation
-    sp.baseScale = 0.3 + Math.random() * 0.4;
-    sp.startX = 200 + Math.random() * 400;
-    sp.arcHeight = Math.random() * 400;
-	sp.velocityX = (Math.random() - 0.5) * 400;
-    // higher arc and smaller coin = slower animation
-    this.duration = (800 + sp.arcHeight * 2) * (1.4 - sp.baseScale);
+	constructor() {
+		super();
+		// Set start and duration for this effect in milliseconds
+		this.start = 0;
+		// Create a sprite
+		let sp = game.sprite("CoinsGold000");
+		// Set pivot to center of said sprite
+		sp.pivot.x = sp.width / 2;
+		sp.pivot.y = sp.height / 2;
+		// Add the sprite particle to our particle effect
+		this.addChild(sp);
+		// randomize startposition, scale, arc and speed on coin animation
+		sp.baseScale = 0.3 + Math.random() * 0.4;
+		sp.startX = 200 + Math.random() * 400;
+		sp.arcHeight = Math.random() * 400;
+		sp.velocityX = (Math.random() - 0.5) * 400;
+		// higher arc and smaller coin = slower animation
+		this.duration = (800 + sp.arcHeight * 2) * (1.4 - sp.baseScale);
 
-    // Save a reference to the sprite particle
-    this.sp = sp;
-  }
-  animTick(nt, lt, gt) {
-    // Every update we get three different time variables: nt, lt and gt.
-    //   nt: Normalized time in procentage (0.0 to 1.0) and is calculated by
-    //       just dividing local time with duration of this effect.
-    //   lt: Local time in milliseconds, from 0 to this.duration.
-    //   gt: Global time in milliseconds,
+		// Save a reference to the sprite particle
+		this.sp = sp;
+	}
+	animTick(nt, lt, gt) {
+		// Every update we get three different time variables: nt, lt and gt.
+		//   nt: Normalized time in procentage (0.0 to 1.0) and is calculated by
+		//       just dividing local time with duration of this effect.
+		//   lt: Local time in milliseconds, from 0 to this.duration.
+		//   gt: Global time in milliseconds,
 
-    // Set a new texture on a sprite particle
-    let num = ("000" + Math.floor(nt * 8)).substr(-3);
-    game.setTexture(this.sp, "CoinsGold" + num);
-    //animate position
-    this.sp.y = 225 + Math.sin(nt * Math.PI) * -this.sp.arcHeight + nt * 400;
-	this.sp.x = this.sp.startX + nt * this.sp.velocityX;
-    // animate scale
-    this.sp.scale.x = this.sp.scale.y = this.sp.baseScale;
-    // Animate alpha
-    this.sp.alpha = nt;
-    // Animate rotation
-    this.sp.rotation = nt * Math.PI * 2;
-  }
+		// Set a new texture on a sprite particle
+		let num = ("000" + Math.floor(nt * 8)).substr(-3);
+		game.setTexture(this.sp, "CoinsGold" + num);
+		//animate position
+		this.sp.y = 225 + Math.sin(nt * Math.PI) * -this.sp.arcHeight + nt * 400;
+		this.sp.x = this.sp.startX + nt * this.sp.velocityX;
+		// animate scale
+		this.sp.scale.x = this.sp.scale.y = this.sp.baseScale;
+		// Animate alpha
+		this.sp.alpha = nt;
+		// Animate rotation
+		this.sp.rotation = nt * Math.PI * 2;
+	}
 }
 
 // ----- End of the assigment ----- //
 
 class Game {
-  constructor(props) {
-    this.totalDuration = 0;
-    this.effects = [];
-    this.renderer = new PIXI.WebGLRenderer(800, 450);
-    document.body.appendChild(this.renderer.view);
-    this.stage = new PIXI.Container();
-    this.loadAssets(props && props.onload);
-  }
-  loadAssets(cb) {
-    let textureNames = [];
-    // Load coin assets
-    for (let i = 0; i <= 8; i++) {
-      let num = ("000" + i).substr(-3);
-      let name = "CoinsGold" + num;
-      let url = "gfx/CoinsGold/" + num + ".png";
-      textureNames.push(name);
-      PIXI.loader.add(name, url);
-    }
-    PIXI.loader.load(
-      function (loader, res) {
-        // Access assets by name, not url
-        let keys = Object.keys(res);
-        for (let i = 0; i < keys.length; i++) {
-          var texture = res[keys[i]].texture;
-          if (!texture) continue;
-          PIXI.utils.TextureCache[keys[i]] = texture;
-        }
-        // Assets are loaded and ready!
-        this.start();
-        cb && cb();
-      }.bind(this),
-    );
-  }
-  start() {
-    this.isRunning = true;
-    this.t0 = Date.now();
-    update.bind(this)();
-    function update() {
-      if (!this.isRunning) return;
-      this.tick();
-      this.render();
-      requestAnimationFrame(update.bind(this));
-    }
-  }
-  addEffect(eff) {
-    this.totalDuration = Math.max(
-      this.totalDuration,
-      eff.duration + eff.start || 0,
-    );
-    this.effects.push(eff);
-    this.stage.addChild(eff);
-  }
-  render() {
-    this.renderer.render(this.stage);
-  }
-  tick() {
-    let gt = Date.now();
-    let lt = (gt - this.t0) % this.totalDuration;
-    for (let i = 0; i < this.effects.length; i++) {
-      let eff = this.effects[i];
-      if (lt > eff.start + eff.duration || lt < eff.start) continue;
-      let elt = lt - eff.start;
-      let ent = elt / eff.duration;
-      eff.animTick(ent, elt, gt);
-    }
-  }
-  sprite(name) {
-    return new PIXI.Sprite(PIXI.utils.TextureCache[name]);
-  }
-  setTexture(sp, name) {
-    sp.texture = PIXI.utils.TextureCache[name];
-    if (!sp.texture) console.warn("Texture '" + name + "' don't exist!");
-  }
+	constructor(props) {
+		this.totalDuration = 0;
+		this.effects = [];
+		this.renderer = new PIXI.WebGLRenderer(800, 450);
+		document.body.appendChild(this.renderer.view);
+		this.stage = new PIXI.Container();
+		this.loadAssets(props && props.onload);
+	}
+	loadAssets(cb) {
+		let textureNames = [];
+		// Load coin assets
+		for (let i = 0; i <= 8; i++) {
+			let num = ("000" + i).substr(-3);
+			let name = "CoinsGold" + num;
+			let url = "gfx/CoinsGold/" + num + ".png";
+			textureNames.push(name);
+			PIXI.loader.add(name, url);
+		}
+		PIXI.loader.load(
+			function (loader, res) {
+				// Access assets by name, not url
+				let keys = Object.keys(res);
+				for (let i = 0; i < keys.length; i++) {
+					var texture = res[keys[i]].texture;
+					if (!texture) continue;
+					PIXI.utils.TextureCache[keys[i]] = texture;
+				}
+				// Assets are loaded and ready!
+				this.start();
+				cb && cb();
+			}.bind(this),
+		);
+	}
+	start() {
+		this.isRunning = true;
+		this.t0 = Date.now();
+		update.bind(this)();
+		function update() {
+			if (!this.isRunning) return;
+			this.tick();
+			this.render();
+			requestAnimationFrame(update.bind(this));
+		}
+	}
+	addEffect(eff) {
+		this.totalDuration = Math.max(
+			this.totalDuration,
+			eff.duration + eff.start || 0,
+		);
+		this.effects.push(eff);
+		this.stage.addChild(eff);
+	}
+	render() {
+		this.renderer.render(this.stage);
+	}
+	tick() {
+		let gt = Date.now();
+		let lt = (gt - this.t0) % this.totalDuration;
+		for (let i = 0; i < this.effects.length; i++) {
+			let eff = this.effects[i];
+			if (lt > eff.start + eff.duration || lt < eff.start) continue;
+			let elt = lt - eff.start;
+			let ent = elt / eff.duration;
+			eff.animTick(ent, elt, gt);
+		}
+	}
+	sprite(name) {
+		return new PIXI.Sprite(PIXI.utils.TextureCache[name]);
+	}
+	setTexture(sp, name) {
+		sp.texture = PIXI.utils.TextureCache[name];
+		if (!sp.texture) console.warn("Texture '" + name + "' don't exist!");
+	}
 }
 
 window.onload = function () {
-  window.game = new Game({
-    onload: function () {
-      game.addEffect(new ParticleSystem());
-    },
-  });
+	window.game = new Game({
+		onload: function () {
+			game.addEffect(new ParticleSystem());
+		},
+	});
 };
